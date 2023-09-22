7e13c69e95032b1d43f80028926caa25b9f69f1f
Luis Gutierrez <luis.gutierrez@themisapp.com.co>
2023-09-22 09:06:01 -0500

    initlibrary

---
+++ projects/ngx-phosphor-icons/src/lib/ngx-phosphor-icons/ngx-phosphor-icons.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
+@NgModule()
+export class NgxPhosphorIconsModule
+{
+    /**
+     * Constructor
+     */
+    constructor(
+        private _domSanitizer: DomSanitizer,
+        private _matIconRegistry: MatIconRegistry
+    )
+    {
+        // Register icon sets
+        this._matIconRegistry.addSvgIconSetInNamespace('ph-bold', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/phosphor-bold.svg'));
+        this._matIconRegistry.addSvgIconSetInNamespace('ph-duotone', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/phosphor-duotone.svg'));
+        this._matIconRegistry.addSvgIconSetInNamespace('ph-light', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/phosphor-light.svg'));
+        this._matIconRegistry.addSvgIconSetInNamespace('ph-regular', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/phosphor-regular.svg'));
+        this._matIconRegistry.addSvgIconSetInNamespace('ph-thin', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/phosphor-thin.svg'));
+        this._matIconRegistry.addSvgIconSetInNamespace('ph-fill', this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/phosphor-fill.svg'));
+    }
+}
